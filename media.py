735466605917cdf8bbd7549d8da3da633f776d1f
Lisa Fedane <26097464+LisaFedane@users.noreply.github.com>
2018-01-31 21:35:58 -0800

    Update media.py

--- media.py
+++ media.py
@@ -1,19 +1,23 @@
-# code is written while taking Udacity course and copying the lesson
-
+# Code is written while taking Udacity course
+# and copied and adapted from the lesson
 import webbrowser
 
+
+# Retrieves and stores movie data
 class Movie():
-    
-    #retrieves and stores data about the movie
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        
+
+    """Stores data about the movie that user inputs."""
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+
+        """Handles for user to use to operate the movie data."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    
-    #opens youtube trailer
-    def show_trailer (self):
+
+    """Opens youtube trailer."""
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
         
 
