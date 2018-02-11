ddf90f55a1611300d7deb6f37576188310a8e4f0
Lisa Fedane <26097464+LisaFedane@users.noreply.github.com>
2018-02-10 23:33:29 -0800

    Update media.py

--- media.py
+++ media.py
@@ -5,18 +5,14 @@ copied and adapted from the lesson
 
 import webbrowser
 
-
-"""
-The class stores information about movies title, storyline,
-movie poster, youtube trailer
-"""
-
-
 class Movie():
-
-    """Constructor to store data about the movie that user inputs."""
+    """
+    The class stores information about movies title, storyline,
+    movie poster, youtube trailer
+    """
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """Constructor to store data about the movie that user inputs."""
 
         """
         Handles for user to use to operate the movie data.
@@ -27,10 +23,10 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    """
-    Opens youtube trailer when given movie name.
-    Ex. movie_name.show_trailer().
-    """
     def show_trailer(self):
+        """
+        Opens youtube trailer when given movie name.
+        Ex. movie_name.show_trailer().
+        """
         webbrowser.open(self.trailer_youtube_url)
         
